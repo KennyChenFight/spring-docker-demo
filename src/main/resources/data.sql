a8c2d0b0f04a9a582e2457089c7ab6d9d084d846
KennyChen <kennychen851228@gmail.com>
2019-09-07 10:25:24 +0800

    更新db設定

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (name, password, email) VALUES ('Kenny', '0000', 'Kenny@example.mail');
+INSERT INTO person (name, password, email) VALUES ('Nicole', '0000', 'Nicole@example.mail');
+INSERT INTO person (name, password, email) VALUES ('Jack', '0000', 'Jack@example.mail');
+INSERT INTO person (name, password, email) VALUES ('Mary', '0000', 'Mary@example.mail');
+INSERT INTO person (name, password, email) VALUES ('David', '0000', 'David@example.mail');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,9 +4,3 @@ CREATE TABLE IF NOT EXISTS person (
     password varchar(100) NOT NULL,
     email varchar(100) NOT NULL
 );
-
-INSERT INTO person (name, password, email) VALUES ('Kenny', '0000', 'Kenny@example.mail');
-INSERT INTO person (name, password, email) VALUES ('Nicole', '0000', 'Nicole@example.mail');
-INSERT INTO person (name, password, email) VALUES ('Jack', '0000', 'Jack@example.mail');
-INSERT INTO person (name, password, email) VALUES ('Mary', '0000', 'Mary@example.mail');
-INSERT INTO person (name, password, email) VALUES ('David', '0000', 'David@example.mail');
